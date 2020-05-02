0bb2b9c5e30a282f60403d3ebea1599572d5fc01
Aleksandr Avilov <52620005+KVEKS@users.noreply.github.com>
2020-05-02 20:01:05 +0300

    Create Day 6. Let's Review.java

---
+++ 30 days of code(Java)/Day 6. Let's Review.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int lines = Integer.parseInt(reader.readLine());
+        for (int j = 0; j < lines; j++) {
+            String left = "";
+            String right = "";
+            String line = reader.readLine();
+            char[] charArr = line.toCharArray();
+            for (int i = 0; i < charArr.length; i++) {
+                if (i % 2 == 0) {
+                    left += charArr[i];
+                } else {
+                    right += charArr[i];
+                }
+            }
+            System.out.println(left + " " + right);
+        }
+        
+    }
+}
